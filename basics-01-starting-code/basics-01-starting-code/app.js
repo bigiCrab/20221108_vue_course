25d896c1f035d81b2c1bbf9d75d0d81d4111bb14
BigiCrab <jerry850408@gmail.com>
2022-11-11 15:02:40 +0800

    18

--- basics-01-starting-code/basics-01-starting-code/app.js
+++ basics-01-starting-code/basics-01-starting-code/app.js
@@ -1,8 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Learn Vue!",
+      courseGoalA: "Learn Vue!",
+      courseGoalB: "Master Vue!",
+      vueLink: "https://vuejs.org/guide/quick-start.html#using-vue-from-cdn",
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNum = Number(Math.random());
+      console.log(this["[[Target]]"]);
+      return randomNum < 0.5 ? this.courseGoalA : this.courseGoalB;
+    },
   },
 });
 
