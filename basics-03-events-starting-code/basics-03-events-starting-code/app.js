9e31b32afeeb5e04212c57753a672554b34e0440
BigiCrab <jerry850408@gmail.com>
2022-11-12 17:31:57 +0800

    24

--- basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -2,11 +2,15 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
     add(num) {
-      this.counter+=num;
+      this.counter += num;
+    },
+    setName(event, postName) {
+      this.name = event.target.value + postName;
     },
   },
 });
