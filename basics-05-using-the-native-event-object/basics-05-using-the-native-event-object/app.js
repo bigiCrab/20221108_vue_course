dfc25b407697a6e8f68d021cf61e79d0df2807a9
BigiCrab <jerry850408@gmail.com>
2022-11-14 18:44:36 +0800

    30

--- basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
@@ -27,13 +27,20 @@ const app = Vue.createApp({
     },
   },
   computed: {
-    fullname(){
-      console.log("🚀 ~ file: app.js ~ line 31 ~ fullname ~ fullname")
+    fullname() {
+      console.log("🚀 ~ file: app.js ~ line 31 ~ fullname ~ fullname");
       if (this.name === "") {
         return "";
       }
       return this.name + " " + "!!!";
-    }
+    },
+  },
+  watch: {
+    counter(value) {
+      if (this.counter >= 30) {
+        this.counter = 0;
+      }
+    },
   },
 });
 
