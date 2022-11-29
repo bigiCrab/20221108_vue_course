6e999aa8e5bac1875d871d139d3fc4e30a2f4922
BigiCrab <jerry850408@gmail.com>
2022-11-29 20:14:43 +0800

    53

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -7,6 +7,7 @@ Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
@@ -16,9 +17,13 @@ Vue.createApp({
     playerHealthStyle() {
       return { width: this.playerHealth + "%" };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       let dmg = getRandomNumber(5, 12);
       this.monsterHealth -= dmg;
       this.attackPlayer();
@@ -26,6 +31,12 @@ Vue.createApp({
     attackPlayer() {
       let dmg = getRandomNumber(8, 15);
       this.playerHealth -= dmg;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      let dmg = getRandomNumber(10, 25);
+      this.monsterHealth -= dmg;
+      this.attackPlayer();
     },
   },
 }).mount("#game");
